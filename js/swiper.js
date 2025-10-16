a1567f0c4a8f499fcf1144442881984156219cd2
KYUUBI-K <sasha06609@gmail.com>
2025-10-16 18:42:59 +0300

    fix

--- js/swiper.js
+++ js/swiper.js
@@ -24,7 +24,6 @@ const productionSwiper = new Swiper(".productions__swiper-slider", {
 
 const partnersSwiper = new Swiper(".partners__swiper-slider", {
   slidesPerView: 4,
-  // simulateTouch: false,
   navigation: {
     nextEl: ".partners__slider__next",
     prevEl: ".partners__slider__prev",
@@ -32,10 +31,7 @@ const partnersSwiper = new Swiper(".partners__swiper-slider", {
 
   breakpoints: {
     1440: { spaceBetween: 28, slidesPerView: 4 },
-    993: {
-      spaceBetween: 27,
-      slidesPerView: 2.52,
-    },
+    993: { spaceBetween: 27, slidesPerView: 2.52 },
     768: { spaceBetween: 28, slidesPerView: 2.52 },
     576: { slidesPerView: 1.79 },
     320: { slidesPerView: 0.9 },
