f8a2b520c9ef56d2abd117c60ad59b043a0cc50c
KYUUBI-K <sasha06609@gmail.com>
2025-10-21 23:18:59 +0300

    fix menu

--- js/menu.js
+++ js/menu.js
@@ -3,14 +3,22 @@ const closeMenuBtn = document.querySelector("[menu-close]");
 const menu = document.querySelector("[menu]");
 const navLinks = document.querySelectorAll(".mobile__menu-link");
 
+const handleOutsideClick = (event) => {
+  if (!menu.contains(event.target) && !openMenuBtn.contains(event.target)) {
+    closeMenu();
+  }
+};
+
 const openMenu = () => {
   menu.classList.add("is-open");
   document.body.classList.add("no-scroll");
+  document.addEventListener("click", handleOutsideClick);
 };
 
 const closeMenu = () => {
   menu.classList.remove("is-open");
   document.body.classList.remove("no-scroll");
+  document.removeEventListener("click", handleOutsideClick);
 };
 
 openMenuBtn.addEventListener("click", openMenu);
@@ -18,18 +26,4 @@ closeMenuBtn.addEventListener("click", closeMenu);
 
 navLinks.forEach((link) => {
   link.addEventListener("click", closeMenu);
-});
-document.addEventListener("click", (event) => {
-  const isMenuOpen = menu.classList.contains("is-open");
-  if (!isMenuOpen) {
-    return;
-  }
-
-  const clickedOutside = !menu.contains(event.target);
-
-  const isClickOnOpenBtn = openMenuBtn.contains(event.target);
-
-  if (clickedOutside && !isClickOnOpenBtn) {
-    closeMenu();
-  }
 });
